8a5031b1f504c223266f9980b03d515589ab062c
LUIS GUSTAVO RODRIGUES BENVINDO <luis.grbenvindo@senacsp.edu.br>
2024-11-21 22:01:04 -0300

    Alterando SAVE da ModelAgendamento

--- model/modelAgendamento.php
+++ model/modelAgendamento.php
@@ -5,22 +5,30 @@ require_once("../services/connectionDB.php");
 
 class modelAgendamento {
     public function save($data){
-        try{
+        try {
+            // Verifique se $data é um array
+            if (!is_array($data)) {
+                throw new Exception("Os dados fornecidos não são um array.");
+            }
+        
             $id_servico = filter_var($data["id_servico"], FILTER_SANITIZE_NUMBER_INT);
-             $data = htmlspecialchars($data["data"],ENT_NOQUOTES);   
-             $hora = htmlspecialchars($data["hora"],ENT_NOQUOTES);
-             $taxi = filter_var($data["taxi"],FILTER_VALIDATE_BOOLEAN);
-            
-             $conn = connectionDB::connect();
-             $save = $conn->prepare("INSERT INTO agendamento(id_servico,data,hora,taxi) VALUES (:id_servico,:data,:hora,:taxi)");
-             $save->bindParam(":id_servico",$id_servico);
-             $save->bindParam(":data",$data);
-             $save->bindParam(":hora",$hora);
-             $save->bindParam(":taxi",$taxi);
-             $save->execute();
-             
+            $data_agendamento = htmlspecialchars($data["data"], ENT_NOQUOTES);
+            $hora = htmlspecialchars($data["hora"], ENT_NOQUOTES);
+            $taxi = filter_var($data["taxi"], FILTER_VALIDATE_BOOLEAN);
+        
+            $conn = connectionDB::connect();
+            $save = $conn->prepare("INSERT INTO agendamento(id_servico, data, hora, taxi) VALUES (:id_servico, :data, :hora, :taxi)");
+            $save->bindParam(":id_servico", $id_servico);
+            $save->bindParam(":data", $data_agendamento);
+            $save->bindParam(":hora", $hora);
+            $save->bindParam(":taxi", $taxi, PDO::PARAM_BOOL); // Adicionei PDO::PARAM_BOOL para garantir que o valor booleano seja tratado corretamente
+            $save->execute();
+        
             return true;
-        }catch(PDOException $e){
+        } catch (PDOException $e) {
+            return false;
+        } catch (Exception $e) {
+            // Captura exceções gerais
             return false;
         }
 
