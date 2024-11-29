704ef46700c8668c8b0ac630102be55562e5de6e
Gabriela Paulino <gabrielappsilva@gamil.com>
2024-11-29 20:40:38 -0300

    Subindo

---
+++ controller/controllerPets.php
@@ -0,0 +1,72 @@
+<?php
+
+include_once("../model/modelPets.php");
+
+class controllerPets {
+
+    public function save($data) {
+        try {         
+            $modelPets = new modelPets();
+            return $modelPets->save($data);
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+
+    public function listAll() {
+        try {
+
+            $modelPets = new modelPets();
+            return $modelPets->listAll();
+
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    public function searchById($id) {
+        try {
+            $modelPets = new modelPets();
+            return $modelPets->searchById($id);
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    public function delete($id) {
+        try {
+            // Validando se o id é um número inteiro
+            if (!is_numeric($id) || $id <= 0) {
+                throw new Exception("Invalid ID");
+            }
+    
+            $modelPets = new modelPets();
+            return $modelPets->delete($id);
+    
+        } catch (Exception $e) {
+            // Log de erro
+            error_log($e->getMessage());
+            return false;
+        }
+    }
+    
+
+    public function update($id, $data) {
+        try {
+            // Verificando se os dados de entrada são válidos
+            if (empty($data["nome"]) || empty($data["sexo"]) || empty($data["nascimento"]) || empty($data["castrado"])) {
+                throw new Exception("Missing required fields");
+            }
+    
+            $modelPets = new modelPets();
+            return $modelPets->update($id, $data);
+        } catch (Exception $e) {
+            // Log do erro
+            error_log($e->getMessage());
+            return false;
+        }
+    }
+       
+
+}
---
+++ pets/update.php
@@ -0,0 +1,28 @@
+<?php
+                                                                     
+require_once("../controller/controllerPets.php");
+require_once("../model/modelPets.php");
+
+if($_SERVER["REQUEST_METHOD"] =="PUT") {
+
+    $query = $_SERVER["QUERY_STRING"];
+    parse_str($query, $params);
+
+    $id = $params["id"];
+
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    $controllerPets = new controllerPets();
+    $update = $controllerPets->update($id, $data);
+
+    if($update) {
+        $msg = array("msg" => "Product has been updated successfully.");
+        echo json_encode($msg);
+    } else {
+        $msg = array("msg" => "Product does not updated.");
+        echo json_encode($msg);
+    }
+
+} else {
+    header("HTTP/1.1 405 Method Not Allowed");
+}
